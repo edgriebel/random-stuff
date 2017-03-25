a6d97152e713f7d530bd99dae94bff96e8f87e4f
Ed Griebel <edgriebel@users.noreply.github.com>
2017-03-25 16:03:07 -0400

    Parameterize and add shebang to image procesor

--- copy_random_images.sh
+++ copy_random_images.sh
@@ -1,3 +1,7 @@
-find kidpix -name \*.jpg -o -name \*.JPG > images.out
-sort -R images_random.out images.out
-cat images_random.out | xargs -L 1 -i cp -n -v \{\} /cygdrive/q
+#!/bin/bash
+IMAGE_ROOT_DIR=kidpix
+IMAGE_DEST_DIR=/cygdrive/q
+
+find $IMAGE_ROOT_DIR -name \*.jpg -o -name \*.JPG > /tmp/images.out
+sort -R /tmp/images_random.out /tmp/images.out
+cat /tmp/images_random.out | xargs -L 1 -i cp -n -v \{\} $IMAGE_DEST_DIR
